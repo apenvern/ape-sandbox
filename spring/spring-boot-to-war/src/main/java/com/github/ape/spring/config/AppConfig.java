1b3b1777e4fba6258a3075d9b13d55b457b5dbee
apenvern <arnaud.pe.it@gmail.com>
2015-06-02 23:20:18 +0200

    spring-boot-to-war : Change configuration run spring-boot app on Tomcat/Jetty

--- spring/spring-boot-to-war/src/main/java/com/github/ape/spring/config/AppConfig.java
+++ spring/spring-boot-to-war/src/main/java/com/github/ape/spring/config/AppConfig.java
@@ -2,29 +2,32 @@ package com.github.ape.spring.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * The class is responsible for the startup and basic configuration of Spring Boot. This is the
- * first entry point for Spring.
- * <p>
- * {@link EnableAutoConfiguration} tells Spring Boot to start adding beans based on classpath
- * settings, other beans, and various property settings.
- * <p>
- * {@link ComponentScan} Configures component scanning directives for use with @Configuration
+ * first entry point for Spring. <br>
+ * {@link EnableAutoConfiguration @EnableAutoConfiguration} tells Spring Boot to start adding beans
+ * based on classpath settings, other beans, and various property settings. <br>
+ * {@link ComponentScan @ComponentScan} Configures component scanning directives for use with @Configuration
  * classes. Provides support parallel with Spring XML's <context:component-scan> element.
  * 
  * @author apenvern
  */
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.github.ape.spring"})
-public class AppConfig {
+public class AppConfig extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(AppConfig.class, args);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(AppConfig.class);
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(AppConfig.class, args);
+  }
 
 }
