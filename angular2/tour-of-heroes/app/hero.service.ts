1b04da1d34fa40479f992fcd4f7046750131fc41
apenvern <arnaud.pe.it@gmail.com>
2016-01-11 01:32:13 +0100

    angular2 - tutorial tour of heros

---
+++ angular2/tour-of-heroes/app/hero.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from 'angular2/core';
+import {HEROES} from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+	
+	getHeroes() {
+		return Promise.resolve(HEROES);
+	}
+
+	// See the "Take it slow" appendix
+	getHeroesSlowly() {
+		return new Promise<Hero[]>(resolve =>
+			setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+		);
+	}
+}
