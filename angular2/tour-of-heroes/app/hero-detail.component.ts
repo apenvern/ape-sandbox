f4f01570e05e4d6e49f4dcacaf526e52823821d3
apenvern <arnaud.pe.it@gmail.com>
2016-01-18 22:42:47 +0100

    angular2 - Next step on tutorial Tour of heros

--- angular2/tour-of-heroes/app/hero-detail.component.ts
+++ angular2/tour-of-heroes/app/hero-detail.component.ts
@@ -1,21 +1,24 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {RouteParams, ROUTER_DIRECTIVES} from 'angular2/router';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
-	selector: 'my-hero-detail',
-	template: `
-		<div *ngIf="hero">
-	    	<h2>{{hero.name}} details!</h2>
-	    	<div><label>id: </label>{{hero.id}}</div>
-	    	<div>
-	    		<label>name: </label>{{hero.name}}
-	    		<div><input [(ngModel)]="hero.name" placeholder="name"></div> 
-	    	</div>
-    	</div>
-    	`,
-  	inputs: ['hero']
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  inputs: ['hero']
 })
+export class HeroDetailComponent implements OnInit {
+  public hero: Hero;
 
-export class HeroDetailComponent {
-	public hero: Hero;
+  constructor(private _heroService: HeroService, private _routeParams: RouteParams) { }
+
+  ngOnInit() {
+    if (!this.hero) {
+      let id = +this._routeParams.get('id');
+      this._heroService.getHero(id).then(hero => this.hero = hero);
+    }
+  }
 }
--- angular2/tour-of-heroes/app/hero.service.ts
+++ angular2/tour-of-heroes/app/hero.service.ts
@@ -3,15 +3,13 @@ import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
-	
 	getHeroes() {
 		return Promise.resolve(HEROES);
 	}
 
-	// See the "Take it slow" appendix
-	getHeroesSlowly() {
-		return new Promise<Hero[]>(resolve =>
-			setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+	getHero(id: number) {
+		return Promise.resolve(HEROES).then(
+			heroes => heroes.filter(h => h.id === id)[0]
 		);
 	}
 }
